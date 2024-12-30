76fef8c77ed9bf05504587dfc48d1dcf2ae83956
Aayan <aayanu52@gmail.com>
2024-12-29 20:11:28 -0800

    Link change a

--- src/app/compont/navbar.tsx
+++ src/app/compont/navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { CarTaxiFront, ChevronDown,  Facebook, Heart, Instagram, Link , Mail, Menu, Phone, Search,  Twitter, Youtube } from "lucide-react";
+import { CarTaxiFront, ChevronDown,  Facebook, Heart, Instagram, Mail, Menu, Phone, Search,  Twitter, Youtube } from "lucide-react";
 import React from "react";
 
  
@@ -21,26 +21,26 @@ export default function Header() {
         {/* Phone */}
         <div className="flex items-center space-x-2 text-white font-bold">
           <Phone className="text-lg" />
-          <Link href="#" className="hover:text-cyan-400">
+          <a href="#" className="hover:text-cyan-400">
           (225) 555-0118
-          </Link>
+          </a>
         </div>
 
         {/* Email */}
         <div className="flex items-center space-x-2 text-white font-bold">
           < Mail className="text-lg" />
-          <Link href="#" className="hover:text-cyan-400">
+          <a href="#" className="hover:text-cyan-400">
           michelle.rivera@example.com
-          </Link>
+          </a>
         </div>
 
         {/* Social MediaIcons */}
         <div className="flex items-center space-x-3 text-white">
           <span className="hover:text-cyan-400">Follow Us:</span>
-          <Link href="#"><Facebook className="text-lg hover:text-cyan-400" /></Link>
-          <Link href="#"><Instagram className="text-lg hover:text-cyan-400" /></Link>
-          <Link href="#">< Youtube className="text-lg hover:text-cyan-400" /></Link>
-          <Link href="#"><Twitter className="text-lg hover:text-cyan-400" /></Link>
+          <a href="#"><Facebook className="text-lg hover:text-cyan-400" /></a>
+          <a href="#"><Instagram className="text-lg hover:text-cyan-400" /></a>
+          <a href="#">< Youtube className="text-lg hover:text-cyan-400" /></a>
+          <a href="#"><Twitter className="text-lg hover:text-cyan-400" /></a>
         </div>
       </div>
 
@@ -54,36 +54,36 @@ export default function Header() {
         
          {/* Navigation as */}
          <nav className="hidden md:flex space-x-6 items-center">
-       <Link href="/"        className="text-slate-600 hover:text-cyan-400">Home</Link>
+       <a href="/"        className="text-slate-600 hover:text-cyan-400">Home</a>
           <div className="flex items-center space-x-1">
-            <Link    href="/shop"  className="text-slate-600 hover:text-cyan-400">
+            <a   href="/shop"  className="text-slate-600 hover:text-cyan-400">
               Shop
-            </Link>
+            </a>
             <ChevronDown className="text-black hover:text-cyan-400" />
           </div>
-           <Link      href="/about" className="text-slate-600 hover:text-cyan-400">about </Link>
-          <Link href='/blog'className="text-slate-600 hover:text-cyan-400">Blog</Link>
-          <Link href="/contact" className="text-slate-600 hover:text-cyan-400">Contact</Link>
-          <Link href="/page" className="text-slate-600 hover:text-cyan-400">Pages</Link>
+           <a     href="/about" className="text-slate-600 hover:text-cyan-400">about </a>
+          <a href='/blog'className="text-slate-600 hover:text-cyan-400">Blog</a>
+          <a href="/contact" className="text-slate-600 hover:text-cyan-400">Contact</a>
+          <a href="/page" className="text-slate-600 hover:text-cyan-400">Pages</a>
         </nav>
 
         {/* Login / Register */}
         <div className="hidden md:flex items-center space-x-2 text-sky-400 font-bold">
-          <Link href="#" className="hover:underline hover:text-cyan-400">
+          <a href="#" className="hover:underline hover:text-cyan-400">
             Login
-          </Link>
+          </a>
           <span>/</span>
-          <Link href="#" className="hover:underline hover:text-cyan-400">
+          <a href="#" className="hover:underline hover:text-cyan-400">
             Register
-          </Link>
+          </a>
         </div>
         
 
         {/* Icons */}
         <div className="flex items-center space-x-4 text-xl text-sky-400">
-          <Link href="#" className="hover:text-cyan-400">
+          <a href="#" className="hover:text-cyan-400">
             <Search />
-          </Link>
+          </a>
           <div className="relative hover:text-cyan-400">
             <Heart />
             <span className="absolute -top-2 -right-3 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
@@ -104,36 +104,36 @@ export default function Header() {
   <SheetContent className="">
    {/* Login / Register */}
    <div className="px-20 gap-1   text-sky-400 font-bold ">
-          <Link href="#" className="hover:underline hover:text-cyan-400 px-1 ">
+          <a href="#" className="hover:underline hover:text-cyan-400 px-1 ">
             Login
-          </Link>
+          </a>
           <span >/</span>
-          <Link href="#" className="hover:underline hover:text-cyan-400 px-1" >
+          <a href="#" className="hover:underline hover:text-cyan-400 px-1" >
             Register
-          </Link>
+          </a>
         </div>
   
     
  {/* Navigation as */}
  <nav className="py-2 ">
-          <Link href="/" className="text-black hover:text-cyan-400 ">Home</Link> <hr></hr> 
+          <a href="/" className="text-black hover:text-cyan-400 ">Home</a> <hr></hr> 
           <div className="flex items-center space-x-1 py-3"> 
-            <Link href="/shop" className="text-slate-600 hover:text-cyan-400  "> 
+            <a href="/shop" className="text-slate-600 hover:text-cyan-400  "> 
               Shop
-            </Link> 
+            </a> 
             <ChevronDown className="text-black hover:text-cyan-400 " />
           </div>  <hr></hr>
           <div className="py-4">
-          <Link href="/about" className="text-slate-600 hover:text-cyan-400 items-center space-x-1 py-4">about</Link> <hr></hr>
+          <a href="/about" className="text-slate-600 hover:text-cyan-400 items-center space-x-1 py-4">about</a> <hr></hr>
           </div> 
           <div className="py-4">
-          <Link href="/blog" className="text-slate-600 hover:text-cyan-400 py-3">Blog</Link> <hr></hr>
+          <a href="/blog" className="text-slate-600 hover:text-cyan-400 py-3">Blog</a> <hr></hr>
          </div > 
          <div className="py-4">
-          <Link href="#" className="text-slate-600 hover:text-cyan-400 py-3">Contact</Link> <hr></hr>
+          <a href="#" className="text-slate-600 hover:text-cyan-400 py-3">Contact</a> <hr></hr>
           </div> 
           <div className="py-4">
-          <Link href="/page" className="text-slate-600 hover:text-cyan-400 ">Pages</Link> <hr></hr>
+          <a href="/page" className="text-slate-600 hover:text-cyan-400 ">Pages</a> <hr></hr>
           </div>
         </nav>
       
